09667cb63b99c99299f906375e147a672870edb4
Wojciech Zakroczymski <iota@iota.software>
2022-10-30 16:08:08 +0100

    Initial Commit

---
+++ RecipesApp2/RecipesApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp2App.swift
+//  RecipesApp2
+//
+//  Created by Wojciech Zakroczymski on 30/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
